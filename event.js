320d28cccff5dbce41159bce416f19b8babbb509
plukenis <ieva.zaboole@gmail.com>
2021-08-04 13:56:04 +0300

    update 08.04

--- event.js
+++ event.js
@@ -1,17 +1,35 @@
 const fs = require("fs");
+
 console.log("pradzia");
-fs.readFile("a.txt", {
-encoding: "utf-8"
+
+fs.readFile("b.txt", {
+    encoding: "utf-8"
 }, function(err, data) {
-if (err) {
-console.log("nepavyko perskaityti failo");
-console.log(err);
-return;
-}
-console.log("perskaiciau faila");
-console.log(data);
+    if (err) {
+        console.log("nepavyko perskaityti failo");
+        console.log(err);
+        return;
+    }
+    console.log("perskaiciau faila b.txt");
+    console.log(data);
 });
+fs.readFile("a.txt", {
+    encoding: "utf-8"
+}, function(err, data) {
+    if (err) {
+        console.log("nepavyko perskaityti failo");
+        console.log(err);
+        return;
+    }
+    console.log("perskaiciau faila a.txt");
+    console.log(data);
+});
+
 setTimeout(function() {
-console.log("dirbu");
-}, 2000);
+    console.log("dirbu");
+}, 3000);
+for (let i = 0; i < 5000000; i++) {
+    for (let j = 0; j < 1000; j++) {
+    }
+}
 console.log("pabaiga");
